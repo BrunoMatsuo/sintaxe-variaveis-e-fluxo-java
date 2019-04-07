748e9c7a071f3057a3a4006bce0875d253818660
Bruno Matsuo <brunocomat@gmail.com>
2019-04-07 19:47:23 -0300

    Adicionando projeto

---
+++ src/TestaMultiplosDe3.java
@@ -0,0 +1,15 @@
+
+public class TestaMultiplosDe3 {
+	
+	public static void main(String[] args) {
+		//for(int numero = 1; numero <= 100; numero++) {
+		//	if(numero % 3 == 0) {
+		//		System.out.println(numero);
+		//	}
+		//}
+		for(int numero = 1; numero <= 33; numero++) {
+			System.out.println(numero*3);
+		}
+	}
+
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,13 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		System.out.println("olá, novo teste");
+		
+		int idade;
+		idade = 33;
+		
+		System.out.println("a idade é " + idade + ", parabéns!");
+	}
+
+}
